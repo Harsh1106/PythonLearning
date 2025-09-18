11607d71478a80e56ce6a2d73e8d5e9dbaee1191
harsh <sinha.harshraj1101@gmail.com>
2025-09-19 01:20:50 +0530

    Operators in Python

---
+++ operators.py
@@ -0,0 +1,18 @@
+print(2 ** 3)  # Exponentiation: 2 raised to the power of 3
+print(10 / 3)  # Division: Standard division
+print(10 // 3)  # Floor Division: Division that results in the largest integer
+print(10 % 3)   # Modulus: Remainder of the division of 10 by 3
+print(5 + 3 * 2)  # Operator Precedence: Multiplication before
+print((5 + 3) * 2)  # Parentheses to change precedence
+print(7 > 5)  # Greater than
+print(5 <= 5)  # Less than or equal to
+print(5 == 5)  # Equality
+print(5 != 3)  # Not equal
+print(True and False)  # Logical AND
+print(True or False)  # Logical OR
+print(not True)  # Logical NOT
+print(5 < 3 or (2 + 2 == 4 and 3 > 1))  # Combined logical expressions
+print(5 < 3 or (2 + 2 == 4 and 3 > 1))  # Combined logical expressions
+
+print('Harsh\'s "laptop"') # Escaping single quote
+print(r'c:\docs\navin')
