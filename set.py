06d8afcacc206a39e9bcbc9a951de91def43cded
harsh <sinha.harshraj1101@gmail.com>
2025-09-21 21:29:17 +0530

    Sets in python

---
+++ set.py
@@ -0,0 +1,10 @@
+# contains unique elements only
+# it doesn't follow the sequence
+# it is mutable
+# it is unordered
+# it uses the concept of hashing for faster access
+set = {25, 14, 98, 63, 75}
+print(set)
+
+set = {25, 14, 98, 63, 75, 14, 98} # duplicates will be ignored
+print(set)
