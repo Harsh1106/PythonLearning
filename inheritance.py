8fef3486962d48440b9edc00e578a5aad33cd69c
harsh <sinha.harshraj1101@gmail.com>
2025-09-24 18:40:34 +0530

    Inheritance in python

---
+++ inheritance.py
@@ -0,0 +1,77 @@
+class Dog:
+    def __init__(self, name):
+        self.name = name
+
+    def display_name(self):
+        print(f"Dog's name is: {self.name}")
+
+class Labrador(Dog): #single inheritance
+    def sound(self):
+        print("Labrador woofs")
+    
+class GuideDog(Labrador): #multilevel inheritance
+    def guide(self):
+        print(f"{self.name}Guides the way!")
+
+class Friendly:
+    def greet(self):
+        print("Friendly")
+
+class GoldenRetrieever(Dog, Friendly): #multiple inheritance
+    def sound(self):
+        print("Golden Retriever barks")
+
+
+lab = Labrador("Buddy")
+lab.display_name()
+lab.sound()
+
+guide = GuideDog("Max")
+guide.display_name()
+guide.sound()
+
+retriever = GoldenRetrieever("Charlie")
+retriever.display_name()
+retriever.greet()
+retriever.sound()
+
+class Animal:
+    def __init__():
+        print("Animal Created")
+
+class Cat(Animal):
+    def __init__(self, name):
+        self.name = name
+    
+    def display(self):
+        print(f"Cat's name is: {self.name}")
+
+class Dog(Animal): #Hierarchical inheritance
+    def __init__(self, name):
+        self.name = name
+    
+    def display(self):
+        print(f"Dog's name is: {self.name}")
+
+cat = Cat("Whiskers")
+cat.display()
+
+dog = Dog("Rex")
+dog.display()
+
+class Lion: #Hybrid inheritance
+    def roar(self):
+        print("Lion roars")
+
+class Tiger(Lion):
+    def growl(self):
+        print("Tiger growls")
+
+class Liger(Tiger, Dog):
+    def display(self):
+        print(f"Liger's name is: {self.name}")
+
+liger = Liger("Leo")
+liger.display()
+liger.roar()
+liger.growl()
