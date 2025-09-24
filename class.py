b11fc1469f17f063bdf58a974fca67c55d4a32ca
harsh <sinha.harshraj1101@gmail.com>
2025-09-24 21:07:53 +0530

    Added Polymorphism and private attribute concepts

--- class.py
+++ class.py
@@ -19,3 +19,14 @@ Dog.species = "Canine" #modifying class attribute
 print(obj1.species)
 print(obj2.species)
 
+class Account:
+    def __init__(self, acc_no, acc_pass):
+        self.acc_no = acc_no #public attribute
+        self.__acc_pass = acc_pass #private attribute
+    def reset_pass(self):
+        print(self.__acc_pass)
+
+acc1 = Account("12335", "abcde")
+print(acc1.acc_no)
+# print(acc1.__acc_pass)
+print(acc1.reset_pass())
