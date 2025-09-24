12bfff5e7b76de7f95422eed0b8bbafded733ce9
harsh <sinha.harshraj1101@gmail.com>
2025-09-24 14:21:21 +0530

    Number Conversions in Python

---
+++ conversion.py
@@ -0,0 +1,9 @@
+print(bin(25)) # conversion from decimal to binary
+print(0b11001) # conversion from binary to decimal
+print(oct(25)) # conversion from decimal to octal
+print(0o31) # conversion from octal to decimal
+print(hex(25)) # conversion from decimal to hexadecimal
+print(0x19) # conversion from hexadecimal to decimal
+print(bin(31))
+print(oct(52))
+print(hex(65))
