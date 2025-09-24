588ffa00270f73275a60be7bdfc203a0c1ab4ee4
harsh <sinha.harshraj1101@gmail.com>
2025-09-24 15:39:19 +0530

    Maths inbuilt function in python

---
+++ maths.py
@@ -0,0 +1,15 @@
+import math # we can also use alias with it such as import math as m here we can use m instead of math
+# from math import sqr, pow # we can also import specific functions from a module
+
+x = math.sqrt(25)
+print(x) # 5.0 return type is float
+
+print(math.floor(2.9)) # 2 return type is int
+
+print(math.ceil(2.1)) # 3 return type is int
+
+print(math.pow(2, 3)) # 8.0 return type is float
+
+print(math.pi)
+
+print(math.e)
